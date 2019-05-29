2c547923dd24b692639aea276d59e888b964e9ae
MrTang <1947168723@qq.com>
2019-05-29 10:00:36 +0800

    auto commit

--- src/com/myoffer/first/QuickSort.java
+++ src/com/myoffer/first/QuickSort.java
@@ -8,23 +8,30 @@ public class QuickSort {
 
 	public static void quickSort(int start, int end, int[] nums) {
 		if (start < end) {
+			//返回关键字排好序的位置
 			int index = partition(start, end, nums);
+			//对左边的序列递归排序
 			quickSort(start, index - 1, nums);
+			//对右边的序列递归排序
 			quickSort(index + 1, end, nums);
 		}
 	}
 
 	private static int partition(int start, int end, int[] nums) {
+		//选取序列中第一个元素为关键字
 		int key = nums[start];
 		int i = start;
 		int j = end;
 		while (i <= j) {
+			//找到第一个大于key的元素
 			while (i < nums.length && nums[i] <= key) {
 				i++;
 			}
+			//找到第一个不大于Key的元素
 			while (j >= 0 && nums[j] > key) {
 				j--;
 			}
+		   //交换
 			if (i < j) {
 				int swap = nums[i];
 				nums[i] = nums[j];
