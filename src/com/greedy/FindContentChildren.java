9f93bc3f8e55af92dd453c109257991412acbf49
MrTang <1947168723@qq.com>
2019-06-01 22:50:18 +0800

    auto commit

---
+++ src/com/greedy/FindContentChildren.java
@@ -0,0 +1,45 @@
+package com.greedy;
+
+import java.util.Arrays;
+
+/**
+ * leetcode 455 描述：假设你是一位很棒的家长， 
+ * 想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+ *  对每个孩子 i ，都有一个胃口值
+ * gi ，这是能让孩子们满足胃口的饼干的最小尺寸； 并且每块饼干 j ，
+ * 都有一个尺寸 sj 。如果 sj >= gi ， 我们可以将这个饼干 j 分配给孩子
+ * i ，这个孩子会得到满足。 你的目标是尽可能满足越多数量的孩子，
+ * 并输出这个最大数值。
+ * 
+ * 示例：[1,2,3],[1,1] 输出：1 解释：你有三个孩子和两块小饼干，
+ * 3个孩子的胃口值分别是：1,2,3。
+ * 虽然你有两块小饼干，由于他们的尺寸都是1， 你只能让胃口值是1的孩子满足。
+ *  所以你应该输出1。
+ * 
+ * @author 汤吉
+ *
+ */
+
+public class FindContentChildren {
+
+	public static int findContentChildren(int[] g, int[] s) {
+		Arrays.sort(g);
+		Arrays.sort(s);
+		int gl = 0;
+		int sl = 0;
+		while (gl < g.length && sl < s.length) {
+			if (s[sl] >= g[gl])
+				gl++;
+			sl++;
+		}
+		return gl;
+	}
+
+	public static void main(String[] args) {
+		int[] chlidrens = { 1, 2 };
+		int[] cookies = { 1, 2, 3 };
+		System.out.println(findContentChildren(chlidrens, cookies));
+
+	}
+
+}
